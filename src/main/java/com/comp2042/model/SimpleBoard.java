70b512d84a407060dec50cc88b722369dbedaf48
Shreya <Shreyasreerajan@gmail.com>
2025-11-18 00:03:59 +0800

    Adjusted brick spawn Y value to delay game over

--- src/main/java/com/comp2042/model/SimpleBoard.java
+++ src/main/java/com/comp2042/model/SimpleBoard.java
@@ -87,7 +87,7 @@ public class SimpleBoard implements Board {
     public boolean createNewBrick() {
         Brick currentBrick = brickGenerator.getBrick();
         brickRotator.setBrick(currentBrick);
-        currentOffset = new Point(4, 10);
+        currentOffset = new Point(4, 2); //Changed y value so that game doesn't end mid screen
         return MatrixOperations.intersect(currentGameMatrix, brickRotator.getCurrentShape(), (int) currentOffset.getX(), (int) currentOffset.getY());
     }
 
